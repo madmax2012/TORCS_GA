5c4a500707f0439f646276c4610265ebd3418ecd
madmax2012 <max@maximilian-mensing.de>
2015-05-24 17:40:35 +0200

    $

--- individual.py
+++ individual.py
@@ -2,12 +2,21 @@ __author__ = 'max'
 class individual(object):
     def __init__(self,p0, p1, p2, p3, p4, p5, p6, p7, p8):
         self.parameters =[]
-        self.parameters.append(p0)
-        self.parameters.append(p1)
-        self.parameters.append(p2)
-        self.parameters.append(p3)
-        self.parameters.append(p4)
-        self.parameters.append(p5)
-        self.parameters.append(p6)
-        self.parameters.append(p7)
-        self.parameters.append(p8)
+        self.parameters.append(''+str(p0)+'')
+        self.parameters.append(""+str(p1)+"")
+        self.parameters.append(""+str(p2)+"")
+        self.parameters.append(""+str(p3)+"")
+        self.parameters.append(""+str(p4)+"")
+        self.parameters.append(""+str(p5)+"")
+        self.parameters.append(""+str(p6)+"")
+        self.parameters.append(""+str(p7)+"")
+        self.parameters.append(""+str(p8)+"")
+        p0 = p0
+        p1 = p1
+        p2 = p2
+        p3 = p3
+        p4 = p4
+        p5 = p5
+        p6 = p6
+        p7 = p7
+        p8 = p8
