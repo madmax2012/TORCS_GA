b245bf21e9fd086407eb0edea9056df6f624bc67
madmax2012 <max@maximilian-mensing.de>
2015-05-25 00:54:01 +0200

    implement crossover

--- GA.py
+++ GA.py
@@ -29,7 +29,7 @@ class RunGA():
             self.cars.append(individual.individual(random.uniform(0,200), random.uniform(0,1), random.uniform(0,20), random.uniform(0,1), random.uniform(0,100), random.uniform(0,1), random.uniform(0,1), random.uniform(0,20), random.uniform(0,1)))
         self.tournamentSize=sp
         self.crossoverChance = cross
-        self.debug=0
+        self.debug=1
 
     def step(self):
         pass
@@ -55,14 +55,14 @@ class gax(RunGA):
 
 
     def step(self):
-        self.tempArray = []
+        self.tempArray = [i for i in range(len(self.cars))]
         self.getFitnessValues()
-        print "printing all fitnesses"
-        self.printFitnessArray()
-        print "\n\n"
-        self.returnFittest()
-        print "fittest at: "+str(self.returnFittest())
-        print "it's values: "+str(self.returnFittestValues())
+   #     print "printing all fitnesses"
+    #    self.printFitnessArray()
+    #    print "\n\n"
+    #    self.returnFittest()
+     #   print "fittest at: "+str(self.returnFittest())
+  #      print "it's values: "+str(self.returnFittestValues())
 
 
         '''keep for parallel function
@@ -78,50 +78,56 @@ class gax(RunGA):
 
 '''
         for replacer in range (len(self.cars)):
-            self.parent1 = np.empty
-            self.parent2 = np.empty
-            self.numberOfElites = (int((1-self.crossoverChance)*self.popsize))+1
-            if  replacer < self.numberOfElites: ##keep the elites
-                self.tempArray.append(self.cars[self.returnFittest()])
-                pass
-            else:
-                for i in range(0,self.tournamentSize):##select parents in tournament
+            print replacer
+           # self.numberOfElites = (int((1-self.crossoverChance)*self.popsize))+1
+           # if  replacer < self.numberOfElites: ##keep the elites
+           #     self.tempArray.append(self.cars[self.returnFittest()])
+           #     pass
+           # else:
+            for i in range(0,self.tournamentSize):##select parents in tournament
+                print "TMMMMMMMMMMMMMMMMM"
+                self.parent1 = np.empty
+                self.parent2 = np.empty
+                self.randomPos = random.randint(0, len(self.fitArray)-1)
+                while(self.fitArray[self.randomPos] == -1):
+                    #print "-1.. get a new one"
                     self.randomPos = random.randint(0, len(self.fitArray)-1)
-                    while(self.fitArray[self.randomPos] == -1):
-                        print "-1.. get a new one"
-                        self.randomPos = random.randint(0, len(self.fitArray)-1)
-                    if self.parent1==np.empty:
-                        self.parent1=self.randomPos
+                if self.parent1==np.empty:
+                    self.parent1=self.randomPos
+                    #self.parent2=self.parent1
+                    #print "our first parents are"+str(self.fitArray[self.parent1])+"  and  "+str(self.fitArray[self.parent2])+""
+                elif self.fitArray[self.randomPos] < self.fitArray[self.parent1]:
+                    if self.fitArray[self.randomPos] == -1:
+                        #print "-1 is no legit parent"
+                        pass
+                    else:
+                        #print "tm: "+str(self.fitArray[self.randomPos])+"  is smaller than  "+str(self.fitArray[self.parent1])+""
                         #self.parent2=self.parent1
-                        #print "our first parents are"+str(self.fitArray[self.parent1])+"  and  "+str(self.fitArray[self.parent2])+""
-                    elif self.fitArray[self.randomPos] < self.fitArray[self.parent1]:
-                        if self.fitArray[self.randomPos] == -1:
-                            print "-1 is no legit parent"
-                        else:
-                            #print "tm: "+str(self.fitArray[self.randomPos])+"  is smaller than  "+str(self.fitArray[self.parent1])+""
-                            #self.parent2=self.parent1
-                            self.parent1=self.randomPos
+                        self.parent1=self.randomPos
+                self.randomPos = random.randint(0, len(self.fitArray)-1)
+                while(self.fitArray[self.randomPos] == -1):
+                   # print "-1.. get a new one"
                     self.randomPos = random.randint(0, len(self.fitArray)-1)
-                    while(self.fitArray[self.randomPos] == -1):
-                        print "-1.. get a new one"
-                        self.randomPos = random.randint(0, len(self.fitArray)-1)
-                    if self.parent2==np.empty:
+                if self.parent2==np.empty:
+                    self.parent2=self.randomPos
+                elif self.fitArray[self.randomPos] < self.fitArray[self.parent2]:
+                    if self.fitArray[self.randomPos] == -1:
+                       # print "-1 is no legit parent"
+                        pass
+                    else:
                         self.parent2=self.randomPos
-                    elif self.fitArray[self.randomPos] < self.fitArray[self.parent2]:
-                        if self.fitArray[self.randomPos] == -1:
-                            print "-1 is no legit parent"
-                        else:
-                            self.parent2=self.randomPos
+            print "now our parents are:"+str(self.fitArray[self.parent1])+"  and  "+str(self.fitArray[self.parent2])+""
 
 
-                print "now our parents are:"+str(self.fitArray[self.parent1])+"  and  "+str(self.fitArray[self.parent2])+""
-
-        ###crossover
-        print "parent1: "+str(self.cars[self.parent1].getParameters())+" has the fitness value "+str(self.fitArray[self.parent1])+""
-        print "parent2: "+str(self.cars[self.parent2].getParameters())+" has the fitness value "+str(self.fitArray[self.parent2])+""
-        #self.printPop()
-        print "collecting now"
-        #print "value0: "+str(self.cars[0].values[0])
+            print "crossover"
+            print self.parent1
+            print self.parent2
+            print "parent1: "+str(self.cars[self.parent1].getParameters())+" has the fitness value "+str(self.fitArray[self.parent1])+""
+            print "parent2: "+str(self.cars[self.parent2].getParameters())+" has the fitness value "+str(self.fitArray[self.parent2])+""
+            self.tempArray[replacer]=individual.individual(self.cars[self.parent1].values[0],self.cars[self.parent2].values[1], self.cars[self.parent1].values[2], self.cars[self.parent2].values[3], self.cars[self.parent1].values[4], self.cars[self.parent2].values[5], self.cars[self.parent1].values[6], self.cars[self.parent2].values[7],self.cars[self.parent1].values[8] )
+            print "child 1 is:"+str(self.tempArray[replacer].getParameters())
+            #self.printPop()
+            #print "value0: "+str(self.cars[0].values[0])
 
         self.currentIteration=self.currentIteration+1
         if self.currentIteration >= self.maxIterations:
