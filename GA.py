f348dafacb6367e19a9e5dee6a5fb5a2f425e07c
madmax2012 <max@maximilian-mensing.de>
2015-05-24 16:58:50 +0200

    generating random driver

--- GA.py
+++ GA.py
@@ -17,12 +17,16 @@ import batch_drive
 import numpy as np
 
 class RunGA():
-    def __init__(self,rep_length, popsize, sp, mut, fitfun, maxgen, cross, nr_processes, run_id , path):
+    def __init__(self, rep_length, popsize, sp, mut, fitfun, maxgen, cross, nr_processes, run_id , path):
 
         self.nr_processes = nr_processes
         self.currentIteration=0
         self.fitfun =fitfun
         self.maxIterations=maxgen
+        self.popsize=popsize
+        self.population = []
+        bla = ["200","0.05","10","0.10","50","0.01","0.01","5", "0.2"]
+        self.driver = "[\""+str(random.uniform(0,200))+"\","+"\""+str(random.uniform(0,1))+"\","+"\""+str(random.uniform(0,20))+"\","+"\""+str(random.uniform(0,1))+"\","+"\""+str(random.uniform(0,100))+"\","+"\""+str(random.uniform(0,1))+"\","+"\""+str(random.uniform(0,1))+"\","+"\""+str(random.uniform(0,20))+"\","+"\""+str(random.uniform(0,1))+"\"]"
 
 
         pass
@@ -60,12 +64,13 @@ class gax(RunGA):
         print "nb of processes: "+str(self.nr_processes)
         print "iteration:        "+str(self.currentIteration)
 
+       # print  "Driver:"+str(self.driver)
 
 
 
        # print self.fitfun(["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"], (self.nr_processes-self.nr_processes)+self.currentIteration)
        # map(self.fitfun, )
-        ba = [["200","0.05","10","0.10","50","0.01","0.01","5", "0.2"],["300","0.05","10","0.10","50","0.01","0.01","5", "0.2"]]
+        ba = [["87.7387897084","0.200804630457","4.20105212553","0.650334796388","12.5515800035","0.250865270024","0.937916693503","14.0768882757","0.612841339211"],["300","0.05","10","0.10","50","0.01","0.01","5", "0.2"]]
         foos = [ba[0],ba[1],["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"],["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"],["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"]]
         bars = [1,2,3]
 
@@ -76,11 +81,12 @@ class gax(RunGA):
 
 
 
+
         self.currentIteration=self.currentIteration+1
         if self.currentIteration >= self.maxIterations:
             print "stop"
             self.stop_reached = True
 
-        def stop(self):
-            return self.stop_reached
-            pass
+    def stop(self):
+        return self.stop_reached
+        pass
--- batch_drive.py
+++ batch_drive.py
@@ -51,7 +51,7 @@ def main():
     sp = 3
     mut = 1./rep_length
     cross = 0.8
-    maxgen = 1
+    maxgen = 3
     run_id = "1"
 
     # set max threads for evaluation
