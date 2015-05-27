7912ec6d6627d29054deb94e4e8622efe9e1467e
madmax2012 <max@maximilian-mensing.de>
2015-05-27 17:43:48 +0200

    -

--- batch_drive.py
+++ batch_drive.py
@@ -52,12 +52,12 @@ def main():
     mut = 1./rep_length
     cross = 0.9
     maxgen = 150
-    onlyThebest = 1
+    onlyThebest = 0
     run_id = "1"
     #debug =0
 
     # set max threads for evaluation
-    nr_processes = 8
+    nr_processes = 10
     fullpath = os.path.abspath(".")
     optimizer = GA.gax(rep_length = rep_length, popsize = popsize, sp = sp, mut = mut, fitfun = evaluation, maxgen = maxgen, cross = cross, nr_processes = nr_processes, run_id = run_id, path = fullpath,onlyThebest=onlyThebest)
     optimizer.run()
