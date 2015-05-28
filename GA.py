9e5fec69cb5267b704da5e8e9865508e9d078610
madmax2012 <max@maximilian-mensing.de>
2015-05-28 10:46:22 +0200

    some cleanup

--- GA.py
+++ GA.py
@@ -12,12 +12,7 @@ import pprocess
 #import matplotlib.pyplot as plt
 import  math
 import individual
-#import pprocess
-#from joblib import Parallel, delayed
-#import multiprocessing
-
-#from datashape.coretypes import String
-#import numpy as np
+import pprocess
 
 class RunGA():
     def __init__(self, rep_length, popsize, sp, mut, fitfun, maxgen, cross, nr_processes, run_id , path, onlyThebest, runval):
@@ -69,40 +64,14 @@ class gax(RunGA):
 
     def step(self):
         self.tempArray = [i for i in range(len(self.cars))]
-        print "before"
         self.getFitnessValues()
-        print "after"
-   #     print "printing all fitnesses"
-    #    self.printFitnessArray()
-    #    print "\n\n"
-    #    self.returnFittest()
-     #   print "fittest at: "+str(self.returnFittest())
-  #      print "it's values: "+str(self.returnFittestValues())
-
-
-        '''keep for parallel function
-        foos = [self.cars[0].getParameters(),["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"],["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"],["100","0.05","10","0.10","50","0.01","0.01","5", "0.2"]]
-        bars = [1,2,3]
-        def maptest(foo):
-            #print foo
-            print"\n\n\n"
-            print self.fitfun(foo, (self.nr_processes-self.nr_processes)+self.currentIteration)
-            print"\n\n\n"
-
-        map(maptest, foos)
-
-'''
         for replacer in range (len(self.cars)):
             self.numberOfElites = (int((1-self.crossoverChance)*self.popsize))+1
-            #print self.numberOfElites
             if  replacer < self.numberOfElites: ##keep the elites
-                #self.tempArray.append(self.cars[self.returnFittest()])
-                # print self.cars[self.returnFittest()].getParameters()
                 self.tempArray[replacer] = self.cars[self.returnFittest()]
                 pass
             else:
                 for i in range(0,self.tournamentSize):##select parents in tournament
-                   # print "TMMMMMMMMMMMMMMMMM"
                     self.parent1 = 9999999
                     self.parent2 = 9999999
                     self.randomPos = random.randint(0, len(self.fitArray)-1)
@@ -111,52 +80,31 @@ class gax(RunGA):
                         self.randomPos = random.randint(0, len(self.fitArray)-1)
                     if self.parent1==9999999:
                         self.parent1=self.randomPos
-                        #self.parent2=self.parent1
-                        #print "our first parents are"+str(self.fitArray[self.parent1])+"  and  "+str(self.fitArray[self.parent2])+""
                     elif self.fitArray[self.randomPos] < self.fitArray[self.parent1]:
                         if self.fitArray[self.randomPos] == -1:
-                            #print "-1 is no legit parent"
                             pass
                         else:
-                            #print "tm: "+str(self.fitArray[self.randomPos])+"  is smaller than  "+str(self.fitArray[self.parent1])+""
-                            #self.parent2=self.parent1
                             self.parent1=self.randomPos
                     self.randomPos = random.randint(0, len(self.fitArray)-1)
                     while(self.fitArray[self.randomPos] == -1):
-                       # print "-1.. get a new one"
                         self.randomPos = random.randint(0, len(self.fitArray)-1)
                     if self.parent2==9999999:
                         self.parent2=self.randomPos
                     elif self.fitArray[self.randomPos] < self.fitArray[self.parent2]:
                         if self.fitArray[self.randomPos] == -1:
-                           # print "-1 is no legit parent"
                             pass
                         else:
                             self.parent2=self.randomPos
-                #print "now our parents are:"+str(self.fitArray[self.parent1])+"  and  "+str(self.fitArray[self.parent2])+""
-
-
-                ### "crossover"
-                #print "parent1: "+str(self.cars[self.parent1].getParameters())+" has the fitness value "+str(self.fitArray[self.parent1])+""
-                #print "parent2: "+str(self.cars[self.parent2].getParameters())+" has the fitness value "+str(self.fitArray[self.parent2])+""
                 self.tempArray[replacer]=individual.individual(self.cars[self.parent1].values[0],self.cars[self.parent2].values[1], self.cars[self.parent1].values[2], self.cars[self.parent2].values[3], self.cars[self.parent1].values[4], self.cars[self.parent2].values[5], self.cars[self.parent1].values[6], self.cars[self.parent2].values[7],self.cars[self.parent1].values[8] )
-                #print "child 1 is:"+str(self.tempArray[replacer].getParameters())
-                #self.printPop()
-                #print "value0: "+str(self.cars[0].values[0])
-        #write fittest back
         for i in range(len(self.cars)):
             self.cars[i] = self.tempArray[i]
 
         for i in range(self.numberOfElites, len(self.cars)):
             for gene in range(9):
                 if (random.uniform(0, 1) <= self.mutationChance):
-                   #print "gene "+str(gene)+" is: "+str(self.cars[i].values[gene])
-                   #print "param "+str(gene)+" is: "+str(self.cars[i].parameters[gene])
                    muval = self.cars[i].values[gene] * random.uniform(-0.1,0.1)
                    self.cars[i].values[gene] = self.cars[i].values[gene] + muval
                    self.cars[i].parameters[gene] = str(float(self.cars[i].parameters[gene]) + muval)
-                   #print "now gene "+str(gene)+" is: "+str(self.cars[i].values[gene])
-                   #print "now param "+str(gene)+" is: "+str(self.cars[i].parameters[gene])
                    pass
 
         print "generation:   "+str(self.currentIteration)
@@ -181,13 +129,7 @@ class gax(RunGA):
         if self.currentIteration >= self.maxIterations:
             self.stop_reached = True
 
-
-
-
-##fittest at: 12
-##it's values: ['190.277074091', '0.362349985495', '9.02742820427', '0.285502127651', '55.2049926684', '0.844332584546', '0.0843421917131', '6.82010070771', '0.966949879827']
     def selection_crossover(self):
-
         pass
 
     def mutation01(self):
@@ -239,9 +181,6 @@ class gax(RunGA):
             elif self.fitArray[i] < self.fitArray[leader]:
                 if self.fitArray[i] != -1:
                     leader = i
-
-           # else:
-           #     print "error"+str(self.fitArray[i])+" not smaller "+str(self.fitArray[leader])
         return leader
 
     def returnFittestValues(self):
@@ -255,16 +194,13 @@ class gax(RunGA):
             self.cars[ind].express()
 
         nproc = self.nr_processes
-
         times = []
         batches = len(self.cars)/nproc+1
-        batch_ranges = range(batches)
-        for batch in batch_ranges:
-            if batch is not batch_ranges[-1]:
+        for batch in range(batches):
+            if batch is not range(batches)[-1]:
                 indivs = [a_+(nproc*batch) for a_ in range(nproc)]
             else:
                 indivs = [a_+(nproc*batch) for a_ in range(len(self.cars)%nproc)]
-            # Start parallel evaluation
             results = pprocess.Map(limit=nproc, reuse=1)
             parallel_function = results.manage(pprocess.MakeReusable(self.fitfun))
             [parallel_function(self.cars[ind].phenotype, (ind-nproc*batch)) for ind in indivs];
