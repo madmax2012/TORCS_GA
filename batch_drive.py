a3c6aae349d73ffea575be6f232f2ee52cc320da
madmax2012 <max@maximilian-mensing.de>
2015-06-10 01:33:59 +0200

    added port to debug output

--- batch_drive.py
+++ batch_drive.py
@@ -54,7 +54,7 @@ def evaluation(parameters, ind):
     for race in range(2):
         for place in range(2):
             print str(root[races[race]][1][0][1][place][0].attrib.get("val"))
-            if root[races[race]][1][0][1][place][0].attrib.get("val") == "scr_server 1":
+            if root[races[race]][1][0][1][place][0].attrib.get("val") in ("scr_server 1", "scr_server 2", "scr_server 3", "scr_server 4", "scr_server 5", "scr_server 6", "scr_server 7", "scr_server 8", "scr_server 9", "scr_server 10", "scr_server 11"):
                placing[race] = place
                print "inf if: "+str(root[races[race]][1][0][1][place][0].attrib.get("val"))+" place: "+str(place)
     time1 = root[2][1][0][1][placing[0]][4].attrib.get("val")
