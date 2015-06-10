4320941789e860268cfed88e5d9624821227f8cd
madmax2012 <max@maximilian-mensing.de>
2015-06-10 16:10:43 +0200

    use noisy sensors

--- batch_drive.py
+++ batch_drive.py
@@ -24,9 +24,9 @@ def evaluation(parameters, ind):
     port = 3000 + ind + 1
     guiless=1
     if (guiless==1):
-        server = subprocess.Popen(["torcs", "-t 1000000000", "-r", fullpath + "/configs/" + str(port) + ".xml"])
+        server = subprocess.Popen(["torcs", "-noisy", "-t 1000000000", "-r", fullpath + "/configs/" + str(port) + ".xml"])
     else:
-        server = subprocess.Popen(["torcs", "-nodamage", fullpath + "/configs/" + str(port) + ".xml"])
+        server = subprocess.Popen(["torcs", "-noisy",  fullpath + "/configs/" + str(port) + ".xml"])
     call_agent = ["python", "snakeoil.py"]
     port = ["-p", str(port)]
     parameters = map(str, parameters)
