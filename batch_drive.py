5b174e832cbb6e8b2a71b06b3ad6a3dbc524363f
madmax2012 <max@maximilian-mensing.de>
2015-06-11 12:08:35 +0200

    10 threads

--- batch_drive.py
+++ batch_drive.py
@@ -81,11 +81,11 @@ def evaluation(parameters, ind):
 
 def main():
     rep_length = 9
-    popsize = 40
+    popsize = 20
     sp = 3
     mut = 1./rep_length
     cross = 0.95
-    maxgen = 20
+    maxgen = 25
     onlyThebest = 0
     run_id = "1"
     #debug =1
