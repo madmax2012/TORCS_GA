4087486d9aefa7f4725f5cb876c6578eada96527
madmax2012 <max@maximilian-mensing.de>
2015-06-12 17:50:05 +0200

    run12

--- batch_drive.py
+++ batch_drive.py
@@ -49,18 +49,18 @@ def evaluation(parameters, ind):
     best_lap_time = root[2][1][0][1][0][6].attrib.get("val")
 
 
-    races = [2,4,5]
-    placing = [-1,-1,-1,-1,-1]
-    for race in range(2):
-        for place in range(2):
-            #print str(root[races[race]][1][0][1][place][0].attrib.get("val"))
-            if root[races[race]][1][0][1][place][0].attrib.get("val") in ("scr_server 1", "scr_server 2", "scr_server 3", "scr_server 4", "scr_server 5", "scr_server 6", "scr_server 7", "scr_server 8", "scr_server 9", "scr_server 10", "scr_server 11"):
-               placing[race] = place
-               #print "inif: "+str(root[races[race]][1][0][1][place][0].attrib.get("val"))+" place: "+str(place)
-    time1 = root[2][1][0][1][placing[0]][4].attrib.get("val")
-    time2 = root[4][1][0][1][placing[1]][4].attrib.get("val")
-    penalty_time1 = root[2][1][0][1][placing[0]][5].attrib.get("val")
-    penalty_time2 = root[2][1][0][1][placing[1]][5].attrib.get("val")
+    #races = [2,4,5]
+    #placing = [-1,-1,-1,-1,-1]
+    #for race in range(2):
+    #    for place in range(2):
+    #        #print str(root[races[race]][1][0][1][place][0].attrib.get("val"))
+    #        if root[races[race]][1][0][1][place][0].attrib.get("val") in ("scr_server 1", "scr_server 2", "scr_server 3", "scr_server 4", "scr_server 5", "scr_server 6", "scr_server 7", "scr_server 8", "scr_server 9", "scr_server 10", "scr_server 11"):
+    #           placing[race] = place
+    #           #print "inif: "+str(root[races[race]][1][0][1][place][0].attrib.get("val"))+" place: "+str(place)
+    #time1 = root[2][1][0][1][placing[0]][4].attrib.get("val")
+    #time2 = root[4][1][0][1][placing[1]][4].attrib.get("val")
+    #penalty_time1 = root[2][1][0][1][placing[0]][5].attrib.get("val")
+    #penalty_time2 = root[2][1][0][1][placing[1]][5].attrib.get("val")
 
 
     #print "now time1: "+str(float(time1))+" Port: "+str(port)
@@ -69,19 +69,25 @@ def evaluation(parameters, ind):
 
     # Check whether the lap was actually finished and verify lap time
     #if laps > 0 and (float(time) - float(best_lap_time) < EPSILON):
-    if (float(root[2][1][0][1][placing[0]][8].attrib.get("val"))<10000.0) and  (float(root[4][1][0][1][placing[0]][8].attrib.get("val"))<10000.0):
-        if float(time1)==0.0 or float(time2)==0.0:
-            time = -1
-        elif (laps > 0 and abs(float(time1) - float(penalty_time1) > EPSILON)) and abs(float(time2) - float(penalty_time2) > EPSILON) and ((float(time1)+float(time2))>20.0):
-           time = float(time1)+float(time2)
-           #print "now timeX: "+str(time)
-        else:
-            time = -1
-    else:
-        time=-1
-        print "to much damage. track 1: "+str(float(root[2][1][0][1][placing[0]][8].attrib.get("val")))
-        print "to much damage. track 2: "+str(float(root[4][1][0][1][placing[0]][8].attrib.get("val")))
-    return time
+   # if (float(root[2][1][0][1][placing[0]][8].attrib.get("val"))<10000.0) and  (float(root[4][1][0][1][placing[0]][8].attrib.get("val"))<10000.0):
+   #     if float(time1)==0.0 or float(time2)==0.0:
+   #         time = -1
+   #     elif (laps > 0 and abs(float(time1) - float(penalty_time1) > EPSILON)) and abs(float(time2) - float(penalty_time2) > EPSILON) and ((float(time1)+float(time2))>20.0):
+   #        time = float(time1)+float(time2)
+   #        #print "now timeX: "+str(time)
+   #     else:
+   #         time = -1
+   # else:
+   #     time=-1
+   #     print "to much damage. track 1: "+str(float(root[2][1][0][1][placing[0]][8].attrib.get("val")))
+   #     print "to much damage. track 2: "+str(float(root[4][1][0][1][placing[0]][8].attrib.get("val")))
+
+   if (laps > 0 and abs(float(time) - float(penalty_time) > EPSILON)):
+        time = float(time)
+   else:
+       time = -1
+
+   return time
 
 def main():
     rep_length = 9
