aa4158da59af624a665160ee6f1122e8b1d210a7
madmax2012 <max@maximilian-mensing.de>
2015-06-12 16:44:11 +0200

    run12

--- GA.py
+++ GA.py
@@ -26,7 +26,7 @@ class RunGA():
         self.onlyTheBest = onlyThebest
         if self.onlyTheBest == 0:
             for i in range(self.popsize):
-                self.cars.append(individual.individual(random.uniform(100,250), random.uniform(0,1), random.uniform(0,20), random.uniform(0,1), random.uniform(0,100), random.uniform(0,1), random.uniform(0,1), random.uniform(0,20), random.uniform(0,1), random.randint(5000,8000)))
+                self.cars.append(individual.individual(250, 0.05, random.uniform(0,20), random.uniform(0,1), random.uniform(0,100), random.uniform(0,1), random.uniform(0,1), random.uniform(0,20), random.uniform(0,1), random.randint(5000,8000)))
 
         if self.onlyTheBest == 1:
             print "runing the best three agents"
