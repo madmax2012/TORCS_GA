2a91edebee499a43ca49cf2ce172bffe6617da1d
max <ccmax0@gmail.com>
2015-06-16 12:40:33 +0200

    new seed

--- batch_drive.py
+++ batch_drive.py
@@ -123,9 +123,9 @@ def main():
     maxgen = 100
     onlyThebest = 0
     run_id = "1"
-    nr_processes =10
+    nr_processes =5
     fullpath = os.path.abspath(".")
-    for runval in range(33, 1000):
+    for runval in range(34, 1000):
         print "run "+str(runval)
         optimizer = GA.gax(rep_length = rep_length, popsize = popsize, sp = sp, mut = mut, fitfun = evaluation, maxgen = maxgen, cross = cross, nr_processes = nr_processes, run_id = run_id, path = fullpath,onlyThebest=onlyThebest, runval=runval)
         optimizer.run()
