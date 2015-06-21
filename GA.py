fe56e24e5d00d29a7bb9698231bedb7bd73201a1
madmax2012 <max@maximilian-mensing.de>
2015-06-21 19:16:50 +0200

    alpineone_modsa

--- GA.py
+++ GA.py
@@ -111,7 +111,7 @@ class gax(RunGA):
         if self.currentIteration == 0:
            print "----------------------------------------------"
            print "run "+str(self.runval)+" starts here"
-        print "run: "+str(self.runval)+" generation: "+str(self.currentIteration)+" fittest at: "+str(self.returnFittest())+" its fitness: "+str(self.returnFittestFitness())
+        print "run; "+str(self.runval)+" ;generation; "+str(self.currentIteration)+" ;fittest at; "+str(self.returnFittest())+" ;its fitness; "+str(self.returnFittestFitness())
         print "it's values:  "+str(self.returnFittestValues())
         print "pop avg fitn: "+str(self.returnAvgFitness())
         print "fitnessarray: "+str(self.fitArray)
@@ -121,11 +121,11 @@ class gax(RunGA):
         os.system(bashCommand)
         print "killed old torcs processes"
 
-        f = open("debugline_forza_modSA_final.csv","a")
-        f.write("run: "+str(self.runval)+" generation: "+str(self.currentIteration)+" fittest at: "+str(self.returnFittest())+" its fitness: "+str(self.returnFittestFitness())+"; number of invalids;"+str(self.invalid)+"\n")
+        f = open("debugline_alpine1_modSA_final.csv","a")
+        f.write("run; "+str(self.runval)+" ;generation; "+str(self.currentIteration)+" ;fittest at; "+str(self.returnFittest())+" ;its fitness; "+str(self.returnFittestFitness())+"; number of invalids;"+str(self.invalid)+"\n")
         f.close
 
-        f = open("foza_modSA_final.csv","a") #opens file with name of "test.txt"
+        f = open("alpine1_modSA_final.csv","a") #opens file with name of "test.txt"
         if self.currentIteration == 0:
             f.write(("\n\n\n\n"))
             f.write(("run "+str(self.runval)+" starts here\n"))
